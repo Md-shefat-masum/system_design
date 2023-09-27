a2b1f0de06b375764b7b05ae1d38f2225ee0679e
Md-shefat-masum <mshefat924@gmail.com>
2023-09-27 16:21:09 +0600

    requirement analysis

---
+++ project.js
@@ -0,0 +1,177 @@
+let project = {
+    name: "account software",
+    date: "2023-09-27",
+    description: "",
+
+    developers: [
+        {
+            id: "",
+            name: "",
+            role: "",
+        },
+    ],
+
+    software_specifications: {
+        feasibility_study: "",
+        system_models: "",
+        functional_requirements: {
+            user_requirements: {
+                client_managers: "",
+                system_end_users: "",
+                client_engineers: "",
+                contrator: "",
+                stystem_architects: "",
+            },
+            system_requriments: {
+                system_end_users: "",
+                client_engineers: "",
+                stystem_architects: "",
+                software_developers: "",
+            }
+        },
+        non_functional_requirements: {
+            product_requirement: {
+                usability: "",
+                efficency_requirement: {
+                    performance: "",
+                    space: "",
+                },
+                depandability: "",
+                security: "",
+            },
+            organizational_requirement: {
+                environmental_requirement: "",
+                operational_requirement: "",
+                development_requirement: "",
+            },
+            external: {
+                regulatory: "",
+                ethical: "",
+                legislative: {
+                    accounting: "",
+                    safety_security: "",
+                }
+            }
+        },
+        software_requirements: "",
+        specifications: "",
+        requirement_elicitation_and_analysis: {
+            requirement_discovery: '',
+            requirement_classification_and_organization: '',
+            requirement_priorities_and_negotiation: '',
+            requirement_specification: '',
+        },
+        requirement_validation: {
+            initial_understanding_of_problem: '',
+            initial_requirement: '',
+            changed_understanding_or_problem: '',
+            changed_requirements: '',
+        },
+        system_requirement_document: {
+            start: "",
+            business_requirement_specification: "",
+            feasibility_study: "",
+            user_requirement_ecitation: "",
+            user_requirement_specification: "",
+            prototyping: "",
+            system_requirement_ecitation: "",
+            system_requirement_specification_and_modeling: "",
+            reviews: "",
+        }
+    },
+
+    client: {
+        name: "",
+        last_meeting_date: "",
+        discussions: [],
+    },
+    
+    modules: [
+        {
+            no: 1,
+            title: "authentication",
+            description: "",
+            meetings: [
+                {
+                    topics: '',
+                    task: '',
+                    discussions: [],
+                    is_with_client: false,
+                    attendent_developers: [],
+                }
+            ],
+            phases: {
+                plan: [
+                    {
+                        no: "",
+                        title: "",
+                        date: "",
+                        is_complete: true,
+                        discussions: [],
+                    }
+                ],
+
+                develop: {
+                    no: 1,
+                    title: "",
+                    estimatted_hours: "",
+                    start_date: "",
+                    end_date: "",
+                    time_tracks: [],
+                    dead_line: "",
+                    is_complete: false,
+                    priority: "height",
+
+                    tests: [
+                        {
+                            no: 1,
+                            title: "",
+                            is_complete: false,
+                        },
+                    ],
+
+                    bugs: [
+                        {
+                            no: 1,
+                            title: "",
+                            description: "",
+                            is_complete: false,
+                            priority: "height",
+                            date: "",
+                        }
+                    ],
+
+                    errors: [
+                        {
+                            no: 1,
+                            title: "",
+                            description: "",
+                            is_complete: false,
+                            priority: "height",
+                            date: "",
+                        }
+                    ],
+                },
+
+                release: {
+                    no: "",
+                    title: "",
+                    date: "",
+                    is_complete: true,
+                    discussions: [],
+                },
+
+                feedbacks: [
+                    {
+                        no: "",
+                        title: "",
+                        date: "",
+                        is_complete: true,
+                        discussions: [],
+                    }
+                ],
+            }
+        }
+    ],
+
+}
