94fe5718f9969c39356f5898def32f5b9b23d2ba
Md-shefat-masum <mshefat924@gmail.com>
2023-10-01 02:05:05 +0600

    auth setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(\App\Http\Controllers\WebsiteController::class)
+    ->prefix('/user')
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::get('/t1', 'index');
+    });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
