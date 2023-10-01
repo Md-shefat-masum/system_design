c9383746ba75910a026a4bc43c270d7f857ca519
Md-shefat-masum <mshefat924@gmail.com>
2023-10-02 05:27:11 +0600

    pagination and passport setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        Paginator::useBootstrap();
     }
 
     /**
@@ -20,5 +25,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Schema::defaultStringLength(191);
+
+        // View::composer('*', function ($view) {
+        //     $app_settings = SettingTitleValue::get();
+        //     $GLOBALS['app_settings'] = $app_settings;
+        //     $view->with([
+        //         'app_settings'=> $app_settings,
+        //     ]);
+        // });
+
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
